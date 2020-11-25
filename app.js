60a4fe52ec90da1f65fc3c3c41ab260d10fe76fb
Nathan Chau <nateychau@gmail.com>
2020-11-25 12:00:48 -0800

    Set up user backend - signup/login routes with session tokens

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+//server
 const express = require("express");
 const app = express();
+
+const port = process.env.PORT || 5000; //production/development ports
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+//db
 const db = require('./config/keys').mongoURI;
 const mongoose = require("mongoose");
 
@@ -8,10 +14,26 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
+//middleware
+const cors = require("cors"); //cors middleware
+const bodyParser = require('body-parser'); //middleware for json parsing
+//user auth
+const users = require("./routes/api/users");
+const passport = require('passport'); //user auth
+require("./config/passport")(passport);
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(cors()); //for any cors issues we might run into with spotify api
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json()); //parse json sent to frontend
+app.use(passport.initialize());
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+//api routes
+const coffee = require("./routes/api/coffee");
+
+//use routes declared in users.js and coffee.js
+app.use("/api/users", users); 
+app.use("/api/coffee", coffee);
+
 
 
---
+++ routes/api/users.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require('../../models/Users');
+
+//form validations 
+const validateSignUpInput = require("../../validation/signup");
+const validateLoginInput = require("../../validation/login");
+
+//user auth
+const bcrypt = require('bcryptjs'); //password hashing
+const jwt = require('jsonwebtoken'); //authorize user sessions
+const keys = require('../../config/keys'); 
+const passport = require('passport'); //passport for auth routes
+
+//sign up route
+router.post("/signup", (req, res) => {
+  const { errors, isValid } = validateSignUpInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then((user) => {
+    //Error if email is already being used, else create new user
+    if (user) {
+      errors.email = "Email is already being used";
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+      });
+
+      //hash user password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => {
+
+              const payload = {
+                id: user.id, 
+                email: user.email, 
+                name: user.name,
+              };
+              //create jwt for session
+              jwt.sign(
+                payload,
+                keys.secretOrKey,
+                { expiresIn: 36000 }, //token expires in 10 hours
+                (err, token) => {
+                  res.json({
+                    success: true,
+                    token: "Bearer " + token,
+                    name: user.name,
+                    email: user.email,
+                  });
+                }
+              );
+            })
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+
+//login route
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then((user) => {
+    if (!user) {
+      errors.email = "Account with this email does not exist";
+      return res.status(400).json(errors);
+    }
+
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        const payload = { 
+          name: user.name,
+          id: user.id, 
+          email: user.email,
+        };
+
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 36000 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token,
+              name: user.name,
+              email: user.email,
+            });
+          }
+        );
+      } else {
+        errors.password = "Incorrect password";
+        return res.status(400).json(errors);
+      }
+    });
+  });
+});
+
+//test route for user auth
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  });
+})
+
+
+module.exports = router;
