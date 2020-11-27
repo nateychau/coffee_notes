d3a6a1902406508e892f7cd501a0d2bfddfcfe6f
Nathan Chau <nateychau@gmail.com>
2020-11-27 12:51:21 -0800

    Add error messages, add password confirmation validation to backend

--- frontend/src/components/user_auth/auth_form.jsx
+++ frontend/src/components/user_auth/auth_form.jsx
@@ -7,7 +7,8 @@ const resetState = {
   password: "",
   passwordConfirm: "",
   name: "",
-}
+  errorList: {},
+};
 
 export class AuthForm extends React.Component {
   constructor(props) {
@@ -18,6 +19,7 @@ export class AuthForm extends React.Component {
       password: "",
       passwordConfirm: "",
       name: "",
+      errorList: {},
     };
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
@@ -25,79 +27,74 @@ export class AuthForm extends React.Component {
     this.switchForm = this.switchForm.bind(this);
   }
 
-  handleChange(event){
+  handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
-  };
+  }
 
-  switchForm(){
-    if(this.state.type === "Sign Up"){
+  switchForm() {
+    if (this.state.type === "Sign Up") {
       this.setState({
         type: "Log In",
-        ...resetState
-        // email: "",
-        // password: "",
-        // passwordConfirm: "",
-        // name: ""
+        ...resetState,
       });
     } else {
       this.setState({
         type: "Sign Up",
-        ...resetState
-        // email: "",
-        // password: "",
-        // passwordConfirm: "",
-        // name: ""
+        ...resetState,
       });
     }
   }
 
-  login(){
-    const payload = { 
-      email: this.state.email, 
-      password: this.state.password, 
+  login() {
+    const payload = {
+      email: this.state.email,
+      password: this.state.password,
     };
-    api.loginUser(payload)
+    api
+      .loginUser(payload)
       .then((res) => {
         authenticateUser(res);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err.response.data);
-      })
+        this.setState({ errorList: err.response.data });
+      });
   }
 
-  signup(){
-    const payload = { 
-      email: this.state.email, 
-      password: this.state.password, 
-      name: this.state.name 
+  signup() {
+    const payload = {
+      email: this.state.email,
+      password: this.state.password,
+      passwordConfirm: this.state.passwordConfirm,
+      name: this.state.name,
     };
-    api.signupUser(payload)
+    api
+      .signupUser(payload)
       .then((res) => {
         authenticateUser(res);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err.response.data);
-      })
-  };
+        this.setState({ errorList: err.response.data });
+      });
+  }
 
   render() {
     let switchText, formAction;
-    if(this.state.type === "Sign Up"){
+    if (this.state.type === "Sign Up") {
       switchText = "Already have an account? Log in instead";
       formAction = this.signup;
     } else {
-      switchText = "Don't have an account? Sign Up"
-      formAction = this.login; 
+      switchText = "Don't have an account? Sign Up";
+      formAction = this.login;
     }
-    
+
     return (
       <div className="auth-body">
         <h2>coffee note</h2>
         <form className="auth-form">
           <h3>{this.state.type}</h3>
-          <label>
-            Email:
-          </label>
+          <label>Email:</label>
           <input
             type="text"
             placeholder="enter email"
@@ -105,9 +102,10 @@ export class AuthForm extends React.Component {
             name="email"
             onChange={this.handleChange}
           ></input>
-          <label>
-            Password:
-          </label>
+          {this.state.errorList.email ? (
+            <div className="auth-error">{this.state.errorList.email}</div>
+          ) : null}
+          <label>Password:</label>
           <input
             type="password"
             placeholder="enter password"
@@ -115,29 +113,29 @@ export class AuthForm extends React.Component {
             name="password"
             onChange={this.handleChange}
           ></input>
-          {this.state.type === "Sign Up" ? 
-          <>
-          <label>
-            Confirm password:
-          </label>
-          <input
-            type="password"
-            placeholder="enter password"
-            value={this.state.passwordConfirm}
-            name="passwordConfirm"
-            onChange={this.handleChange}
-          ></input>
-          <label>
-            Your name:
-          </label>
-          <input
-            type="text"
-            placeholder="enter name"
-            value={this.state.name}
-            name="name"
-            onChange={this.handleChange}
-          ></input>
-          </> : null}
+          {this.state.errorList.password ? (
+            <div className="auth-error">{this.state.errorList.password}</div>
+          ) : null}
+          {this.state.type === "Sign Up" ? (
+            <>
+              <label>Confirm password:</label>
+              <input
+                type="password"
+                placeholder="enter password"
+                value={this.state.passwordConfirm}
+                name="passwordConfirm"
+                onChange={this.handleChange}
+              ></input>
+              <label>Your name:</label>
+              <input
+                type="text"
+                placeholder="enter name"
+                value={this.state.name}
+                name="name"
+                onChange={this.handleChange}
+              ></input>
+            </>
+          ) : null}
         </form>
         <button onClick={formAction}>{this.state.type}</button>
         <button onClick={this.switchForm}>{switchText}</button>
@@ -145,4 +143,3 @@ export class AuthForm extends React.Component {
     );
   }
 }
-
