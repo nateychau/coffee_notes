69e209c9c62cf9287d0cf71abe1b7cc3fd98cf8d
Nathan Chau <nateychau@gmail.com>
2020-12-03 16:42:27 -0800

    Updates to bean details page

--- frontend/src/components/beans/bean_detail/static_detail.jsx
+++ frontend/src/components/beans/bean_detail/static_detail.jsx
@@ -38,19 +38,21 @@ export const StaticDetail = ({ bean, notes, handleDelete, handleEdit }) => {
             </>
           ) : null}
         </div>
+        <h2 className="bean-name">Name</h2>
+        <div className="bean-name">{bean.name}</div>
         <div className="detail-text">
-          <h2>Name</h2>
-          <div>{bean.name}</div>
-          <h2>Roaster</h2>
-          <div>{bean.roaster}</div>
-          <h2>Place of Origin</h2>
-          <div>{bean.origin}</div>
-          <h2>Roast</h2>
-          <div>{bean.roast}</div>
-          <h2>Rating</h2>
-          <div>{bean.rating}</div>
+          <div className="detail-text-left">
+            <h2>Roaster</h2>
+            <div>{bean.roaster}</div>
+            <h2>Place of Origin</h2>
+            <div>{bean.origin}</div>
+            <h2>Roast</h2>
+            <div>{bean.roast}</div>
+            <h2>Rating</h2>
+            <div>{bean.rating}</div>
+          </div>
           {notes ? (
-            <>
+            <div className="detail-text-right">
               <h2> Brew Method </h2>
               <div>{notes.brewMethod}</div>
               <h2>Brew Time</h2>
@@ -59,8 +61,13 @@ export const StaticDetail = ({ bean, notes, handleDelete, handleEdit }) => {
               <div>{notes.ratio}</div>
               <h2>Personal Notes</h2>
               <div>{notes.notes}</div>
-            </>
-          ) : null}
+            </div>
+          ) : 
+          (
+            <div className="detail-text-right no-note">
+              No entries yet
+            </div>
+          )}
         </div>
         {notes ? (
           <Link className="brewEntries" to={`/notes/bean/${notes.beanId}`}>
