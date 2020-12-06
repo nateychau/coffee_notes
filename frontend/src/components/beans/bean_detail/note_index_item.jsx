9277baf66354839d8b86e24317609bd8b4beb323
Maxim Chen <chenr.maxim@gmail.com>
2020-12-05 22:08:50 -0800

    fixed sizing on .note-item-container/wrapper

--- frontend/src/components/beans/bean_detail/note_index_item.jsx
+++ frontend/src/components/beans/bean_detail/note_index_item.jsx
@@ -9,23 +9,23 @@ export const NoteIndexItem = ({ note }) => {
   return (
     <li className="note-item-container">
       <div className="note-container">
-        <div>
+        <div className="note-wrapper">
           <div className="note-item-header"> Date </div>
           <div className="note-item-text">{fixTimeStamp(note)}</div>
         </div>
-        <div>
+        <div className="note-wrapper">
           <div className="note-item-header"> Brew Method </div>
           <div className="note-item-text">{note.brewMethod}</div>
         </div>
-        <div>
+        <div className="note-wrapper">
           <div className="note-item-header"> Brew Time </div>
           <div className="note-item-text">{note.time}</div>
         </div> 
-        <div>
+        <div className="note-wrapper">
           <div className="note-item-header"> Coffee to Water Ratio </div>
           <div className="note-item-text">{note.ratio}</div>
         </div>
-        <div>
+        <div className="note-wrapper">
           <div className="note-item-header"> Notes </div>
           <div className="note-item-text">{note.notes}</div>
         </div>
