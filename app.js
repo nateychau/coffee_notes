48f298f9c0bbca946e52b300265bb4bd09e12e4b
Maxim Chen <chenr.maxim@gmail.com>
2021-01-13 18:31:10 -0800

    fix express build paths

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
