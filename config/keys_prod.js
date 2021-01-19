52b180478f1c38b695a669cfd94f8a7d3d722ded
Nathan Chau <nateychau@gmail.com>
2021-01-18 20:23:14 -0800

    Test

---
+++ config/keys_prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongoURI: process.env.mongoURI,
+  secretOrKey: process.env.secretOrKey,
+  AWSaccessKeyId: process.env.AWSaccessKeyId,
+  AWSsecretAccessKey: process.env.AWSaccessKeyId,
+  BUCKET_NAME: "coffee-notes-dev",
+  spotifyClientID: process.env.spotifyClientID,
+  spotifyClientSecret: process.env.spotifyClientSecret,
+};
